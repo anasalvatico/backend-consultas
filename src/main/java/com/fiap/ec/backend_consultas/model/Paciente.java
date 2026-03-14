a702c63a7826610ec329fd4e227df4cf9ce18b0c
anasalvatico <anaclarasalvatico@gmail.com>
2026-03-14 11:24:52 -0300

    fix: adiciona constraints nullable e unique no model Paciente

--- src/main/java/com/fiap/ec/backend_consultas/model/Paciente.java
+++ src/main/java/com/fiap/ec/backend_consultas/model/Paciente.java
@@ -1,7 +1,5 @@
 package com.fiap.ec.backend_consultas.model;
-
 import jakarta.persistence.*;
-
 import java.time.LocalDate;
 
 @Entity
@@ -13,13 +11,14 @@ public class Paciente {
     private Long id;
     @Column(nullable = false)
     private String nome;
+    @Column(nullable = false, unique = true)
     private String cpf;
+    @Column(nullable = false)
     private String email;
     private String telefone;
     private LocalDate dataNascimento;
     private boolean ativo;
 
-    public Paciente() {}
     public Paciente(String nome, String cpf,String email, String telefone, LocalDate dataNascimento, boolean ativo ) {
         this.nome = nome;
         this.cpf = cpf;
@@ -35,7 +34,7 @@ public class Paciente {
     public String getEmail() {return email; }
     public String getTelefone() {return telefone;}
     public LocalDate getDataNascimento() {return dataNascimento;}
-    public boolean isAtivo() {return ativo;}
+    public boolean getAtivo() {return ativo;}
 
     public void setNome(String nome) { this.nome = nome; }
     public void setCpf(String cpf) {this.cpf = cpf;}
