129dc86c015de6ec247cc5d73e7256d583ab97c0
anasalvatico <anaclarasalvatico@gmail.com>
2026-03-14 11:02:30 -0300

    feat: criação do service de Paciente

---
+++ src/main/java/com/fiap/ec/backend_consultas/service/PacienteService.java
@@ -0,0 +1,21 @@
+package com.fiap.ec.backend_consultas.service;
+import com.fiap.ec.backend_consultas.model.Paciente;
+import com.fiap.ec.backend_consultas.repository.PacienteRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class PacienteService {
+    private PacienteRepository repository;
+    public PacienteService(PacienteRepository repository) {
+        this.repository = repository;
+    }
+
+    public Paciente salvar(Paciente paciente) {
+        return repository.save(paciente);
+    }
+
+    public List<Paciente> listar() {
+        return repository.findAll();
+    }
+}
